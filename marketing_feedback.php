aa7f0f2b473479e21414b873a9c90c9838d1c5cc
Senuthi Amalmee Andaraarachchi <senuthi.uog04@edu.lnbti.lk>
2026-02-02 00:33:53 +0530

    edit package page and feedback page

--- marketing_feedback.php
+++ marketing_feedback.php
@@ -913,7 +913,7 @@ $footerLinks = [
                      <a href="marketing_feedback.php" class="text-amber-600 transition-all duration-300 relative group">
                         <i class="fas fa-user-check text-xs text-amber-500 mr-2"></i>
                         Customer Feedback
-                        <span class="absolute -bottom-1 left-0 w-full h-0.5 bg-amber-500"></span>
+                        <span class="absolute -bottom-1 left-0 w-0 h-0.5 bg-amber-500 group-hover:w-full transition-all duration-300"></span>
                     </a>
                 </div>
 
